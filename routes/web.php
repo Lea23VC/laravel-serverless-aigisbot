66626b8d146e0752deca518e03e18423aadf781b
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-08-28 22:54:30 -0400

    refactor: remove filament socialite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Log;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('auth0')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('auth0')->user();
+
+    Log::info("user after callback: " . $user->id);
+});
 
 Route::get('/', function () {
     return view('welcome');
