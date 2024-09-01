d92171a2e7cc2cc5a885c6a807be5d975db1ebb1
Lea23VC <leandro.villalobos.dx@gmail.com>
2024-08-31 23:10:17 -0400

    refactor: update config for new server

--- pass_env_to_ssm.py
+++ pass_env_to_ssm.py
@@ -3,30 +3,43 @@ import subprocess
 
 # Load environment variables from .env file
 env_vars = {}
-if os.path.exists('.env'):
-    with open('.env', 'r') as env_file:
+if os.path.exists(".env"):
+    with open(".env", "r") as env_file:
         for line in env_file:
             line = line.strip()
-            if '=' in line:
-                key, value = line.split('=', 1)
+            if "=" in line:
+                key, value = line.split("=", 1)
                 env_vars[key] = value.strip().strip("'")
+
 
 # Command to create individual secrets from .env file
 def create_secrets():
-    print(f"Creating secrets for {os.environ.get('OWNER')}/{os.environ.get('REPO_NAME')}...")
+    print(
+        f"Creating secrets for {os.environ.get(
+            'OWNER')}/{os.environ.get('REPO_NAME')}..."
+    )
 
     for key, value in env_vars.items():
         print(f"Creating secret {key} with value: {value}...")
-        subprocess.run([
-            'aws', 'ssm', 'put-parameter',
-            '--profile', 'default',
-            '--name', f"/aigisbot/prod/{key}",
-            '--value', value,
-            '--type', 'SecureString',
-            '--overwrite'
-        ])
+        subprocess.run(
+            [
+                "aws",
+                "ssm",
+                "put-parameter",
+                "--profile",
+                "new-lea",
+                "--name",
+                f"/aigisbot/prod/{key}",
+                "--value",
+                value,
+                "--type",
+                "SecureString",
+                "--overwrite",
+            ]
+        )
 
     print("Secrets created successfully.")
+
 
 if __name__ == "__main__":
     create_secrets()
